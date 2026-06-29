70919d516761ea4d39f92c463cb6e0d2ec91f6fb
Swathi-prasanna <swathi@paithacs.in>
2026-06-29 20:09:41 +0530

    docotr-type

---
+++ patient/src/assets/Medical Reports.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MedicalReports = () => {
+  return (
+    <div>
+      <div className="medical-page py-4">
+        <div className="mb-4">
+            <h2 classname="medical-heading">"Medical Records"</h2>
+            <p className="medical-subheading">"Access your complete medical history"</p>
+      </div>
+    </div>
+  );
+};
+
+export default MedicalReports;

1b79ca312ac655257e980a0e839bd586ad8561c6
Swathi-prasanna <swathi@paithacs.in>
2026-06-30 11:52:23 +0530

    BookAppointement

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import PatientSidebar from "./assets/PatientSidebar";
 import DashboardPage from "./assets/DashboardPage";
 import Prescriptions from "./assets/Prescriptions";
+import MedicalReport from "./assets/MedicalReports";
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<DashboardPage />} />
           <Route path="/PrescriptionPage" element={<Prescriptions />} />
+          <Route path="/Inventorypage" element={<MedicalReport />}  />
+          <Route path="/DispensingPage" element={<Prescriptions />} />
+
         </Routes>
       </div>
     </div>
